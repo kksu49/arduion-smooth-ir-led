f1b0eb2eb770f94e69855ea7203f1de9f4c95865
Kevin Su <zhousu@Kevins-MacBook-Pro.local>
2014-03-05 11:21:02 -0500

    Initial Commit

---
+++ firstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firstApp
+//
+//  Created by Kevin Su on 3/5/14.
+//  Copyright (c) 2014 Kevin Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ firstApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  firstApp
+//
+//  Created by Kevin Su on 3/5/14.
+//  Copyright (c) 2014 Kevin Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
